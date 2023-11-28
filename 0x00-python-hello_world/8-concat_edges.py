990eaac520a69eea71f2dcc95d7192afd42f78a4
yassineboucham <yassineboucham11@gamil.com>
2023-11-28 15:05:21 +0000

    8

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
-language that combines remarkable power with ver yclear syntax"
-print(str[39:39+27] + str[106:107+5] + str[:6])
-print('\n')
+ language that combines remarkable power with ver yclear syntax"
+str = str[39:39+27] + str[106:107+5] + str[:6]
+print(str)
