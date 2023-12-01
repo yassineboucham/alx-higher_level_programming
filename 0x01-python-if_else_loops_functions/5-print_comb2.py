a9bd2082a4e0c5dfddaba5155602bc18d39ce8e5
yassineboucham <yassineboucham11@gamil.com>
2023-12-01 14:08:15 +0000

    5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 for i in range(00, 100):
     if (i == 99):
-        print(i)
-    print(f"{i:02d}, ", end='')
-    
+        print("{}".format(i))
+    print("{:02d}, ".format(i), end='')
