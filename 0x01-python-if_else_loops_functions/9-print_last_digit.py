74e1550c3c4569da4f8e5f0b9a14b31afa015706
yassineboucham <yassineboucham11@gamil.com>
2023-12-01 14:52:35 +0000

    all

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def print_last_digit(number):
+    if (number > 0):
+        last_degit = number % 10
+    else:
+        last_degit = (number * -1) % 10
+    print("{}".format(last_degit), end="")
+    return (last_degit)
