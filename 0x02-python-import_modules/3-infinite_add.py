1565be1492b61d1a0d66f6c907fe72dc6e279e14
yassineboucham <yassineboucham11@gamil.com>
2023-12-03 12:04:20 +0000

    3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-res = 0
-import sys
-vars = sys.argv[1:]
-for i in vars:
-    res += int(i)
-print(res)
+if __name__ == "__main__":
+    res = 0
+    import sys
+    vars = sys.argv[1:]
+    for i in vars:
+        res += int(i)
+    print("".format(res))
