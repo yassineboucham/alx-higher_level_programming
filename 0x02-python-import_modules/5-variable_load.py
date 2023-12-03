a5c41c68a6241ea6ed0dcd922bec2bb2e1b73bf1
yassineboucham <yassineboucham11@gamil.com>
2023-12-03 12:16:13 +0000

    5

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import variable_load_5.py
+    import variable_load_5
     if __name__ == "__main__":
-        print(variable_load_5.py.a)
+        print("{}".format(variable_load_5.a))
