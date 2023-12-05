7da8c9ece9402a9eaaa7e5bad9eb80bf0780caf6
yassineboucham <yassineboucham11@gamil.com>
2023-12-05 16:35:21 +0000

    i thin it dane 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,8 +3,5 @@ def print_matrix_integer(matrix=[[]]):
     if not matrix:
         return None
     for row in matrix:
-        if len(row) == 0:
-            print()
-        for element in row:
-            print("{:d}".format(element), end = " ")
-    print('')
+        for i in range(len(row)):
+            print("{:d}".format(row[i]), end = " " if i < len(row) - 1 else "\n")
