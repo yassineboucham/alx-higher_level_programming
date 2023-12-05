85ff46405fdc35cd1c1fa4d74dab593f8bb8317b
yassineboucham <yassineboucham11@gamil.com>
2023-12-05 16:43:11 +0000

    i think it don 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     if not matrix:
         return None
     for row in matrix:
+        if not row:
+            print()
         for i in range(len(row)):
             print("{:d}".format(row[i]), end=" " if i < len(row) - 1 else "\n")
