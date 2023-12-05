a1b8ab67f8948983f5ff55ad5b5c1df83a983751
yassineboucham <yassineboucham11@gamil.com>
2023-12-05 17:12:48 +0000

    solve 11

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    del my_list[idx]
+    if len(my_list) is idx:
+        my_list[:] = my_list[:idx - 1]
+    else:
+        del my_list[idx]
     return my_list
