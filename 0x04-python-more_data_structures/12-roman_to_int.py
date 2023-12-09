e202e081f5ed6d2b3f40fbe68c8f629a29168a86
yassineboucham <yassineboucham11@gamil.com>
2023-12-09 17:35:23 +0000

    12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,24 +3,28 @@ def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
     number = 0
-    for i in range(len(roman_string)):
-        if (roman_string[i] == 'I'):
-            number += 1
-        elif (roman_string[i] == 'V'):
-            number += 5
-        elif (roman_string[i] == 'X'):
-            number += 10
-        elif (roman_string[i] == 'L'):
-            number += 50
-        elif (roman_string[i] == 'C'):
-            number += 100
-        elif (roman_string[i] == 'D'):
-            number += 500
-        elif (roman_string[i] == 'M'):
-            number += 1000
-        else:
-            if (i > len(roman_string) - 1 and roman_string[i : i + 2] == 'IV'):
-                number += 4
-            elif (i > len(roman_string) - 1 and roman_string[i : i + 2] == 'IX'):
-                number += 9
+    i = 0
+    while i < len(roman_string):
+        if (i < len(roman_string) - 1 and roman_string[i : i + 2] == 'IV'):
+            number += 4
+            i += 2
+        elif (i < len(roman_string) - 1 and roman_string[i : i + 2] == 'IX'):
+            number += 9
+            i += 2
+        if (i < len(roman_string)):
+            if (roman_string[i] == 'I'):
+                number += 1
+            elif (roman_string[i] == 'V'):
+                number += 5
+            elif (roman_string[i] == 'X'):
+                number += 10
+            elif (roman_string[i] == 'L'):
+                number += 50
+            elif (roman_string[i] == 'C'):
+                number += 100
+            elif (roman_string[i] == 'D'):
+                number += 500
+            elif (roman_string[i] == 'M'):
+                number += 1000
+            i += 1
     return number
