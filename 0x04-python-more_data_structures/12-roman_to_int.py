fc11337259929ff43e3e10e9eddcc0adb2676ed3
yassineboucham <yassineboucham11@gamil.com>
2023-12-09 17:39:49 +0000

    12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,26 +5,26 @@ def roman_to_int(roman_string):
     number = 0
     i = 0
     while i < len(roman_string):
-        if (i < len(roman_string) - 1 and roman_string[i : i + 2] == 'IV'):
+        if i < len(roman_string) - 1 and roman_string[i: i + 2] == 'IV':
             number += 4
             i += 2
-        elif (i < len(roman_string) - 1 and roman_string[i : i + 2] == 'IX'):
+        elif i < len(roman_string) - 1 and roman_string[i: i + 2] == 'IX':
             number += 9
             i += 2
-        if (i < len(roman_string)):
-            if (roman_string[i] == 'I'):
+        else:
+            if roman_string[i] == 'I':
                 number += 1
-            elif (roman_string[i] == 'V'):
+            elif roman_string[i] == 'V':
                 number += 5
-            elif (roman_string[i] == 'X'):
+            elif roman_string[i] == 'X':
                 number += 10
-            elif (roman_string[i] == 'L'):
+            elif roman_string[i] == 'L':
                 number += 50
-            elif (roman_string[i] == 'C'):
+            elif roman_string[i] == 'C':
                 number += 100
-            elif (roman_string[i] == 'D'):
+            elif roman_string[i] == 'D':
                 number += 500
-            elif (roman_string[i] == 'M'):
+            elif roman_string[i] == 'M':
                 number += 1000
             i += 1
     return number
