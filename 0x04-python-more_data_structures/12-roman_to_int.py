8d1b2403278f6dd9657f98897d1625cbac877d45
yassineboucham <yassineboucham11@gamil.com>
2023-12-09 17:41:39 +0000

    12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
-        return 0
+        return None
     number = 0
     i = 0
     while i < len(roman_string):
