d649fcab67adb952a0cf3db98a710c0b289067c5
yassineboucham <yassineboucham11@gamil.com>
2023-12-11 13:08:08 +0000

    all

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,3 +4,4 @@ if __name__ == "__main__":
     for name in sorted(dir(hidden_4)):
         if name[:2] != '__':
             print("{}".format(name))
+            
--- 0x03-python-data_structures/6-main.py
+++ 0x03-python-data_structures/6-main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 print_matrix_integer = __import__('6-print_matrix_integer').print_matrix_integer
 
-matrix = [[1, 2], [4, 5]]
+matrix = [[]]
 
 print_matrix_integer(matrix)
 print("--")
