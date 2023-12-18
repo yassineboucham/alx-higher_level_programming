cf4a96208ddff5bce4cd285e844ad087cdbe7a26
yassineboucham <yassineboucham11@gamil.com>
2023-12-18 20:06:59 +0000

    all

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,9 +3,11 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i in range(x):
             print("{}".format(my_list[i]), end="")
-        print("nb_print: {}".format(my_list[x]))
+        print()
+        return (my_list[x - 1])
     except IndexError:
+        j = 0
         for list in my_list:
             j += 1
-            print("{}".format(list), end="")
-        print("nb_print: {}".format(my_list[j]))
+        print()
+        return (list)
--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        res = a/b
+    except (ZeroDivisionError, TypeError):
+        res = None
+    finally:
+        print("Inside result: {}".format(res))
+        return res
