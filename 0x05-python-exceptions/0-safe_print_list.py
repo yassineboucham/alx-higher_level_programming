aa18667fd7dbb29b1ea872f7e29a76c7b2dbe71c
yassineboucham <yassineboucham11@gamil.com>
2023-12-19 09:45:32 +0000

    0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print("{}".format(my_list[i]), end="")
         print()
-        return (my_list[x - 1])
+        return (my_list[x - 1] if x != 0 else my_list[0])
     except IndexError:
         j = 0
         for list in my_list:
