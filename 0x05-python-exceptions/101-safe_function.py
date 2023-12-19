7d317b829d2329d1043ad84b2e251f3f9c515b1d
yassineboucham <yassineboucham11@gamil.com>
2023-12-19 09:30:59 +0000

    all

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+def safe_function(fct, *args):
+    res = 0
+    try:
+        res = fct(*args)
+    except (ZeroDivisionError, IndexError) as e:
+        print("Exception: ", e, file=sys.stderr)
+        res = None
+    return (res)
