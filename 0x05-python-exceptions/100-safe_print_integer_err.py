b7703d0efe79b98c8f9199fec8d38320513e6ba0
yassineboucham <yassineboucham11@gamil.com>
2023-12-19 10:06:11 +0000

    7

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,7 +6,7 @@ def safe_print_integer_err(value):
     is_int = True
     try:
         print("{:d}".format(value))
-    except TypeError as e:
+    except (TypeError, ValueError) as e:
         print("Exception:", e, file=sys.stderr)
         is_int = False
     return is_int
