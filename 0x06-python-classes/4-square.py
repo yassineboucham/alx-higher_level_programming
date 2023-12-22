9605beb18173e892c1400cf5c567f14e869bcade
yassineboucham <yassineboucham11@gamil.com>
2023-12-22 14:36:12 +0000

    all

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -7,18 +7,18 @@ class Square():
 
     def size(self, value):
         """setter """
-        self.__size = value
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
 
     def size(self):
         """getter"""
         return self.__size
 
     def __init__(self, size=0):
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
             self.__size = size
 
     def area(self):
