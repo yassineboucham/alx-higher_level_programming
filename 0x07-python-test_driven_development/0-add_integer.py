5de2750ae3a8b86de51ce20830e41057b644bff7
yassineboucham <yassineboucham11@gamil.com>
2024-01-04 12:30:38 +0000

    0, 2

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def add_integer(a, b=98):
+    """
+    add tow numbers
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("a must be an integer")
+    return int(a + b)
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/5-text_indentation.txt")
---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def matrix_divided(matrix, div):
+    divs = []
+    for mat in matrix:
+        if len(mat) != len(matrix[0]):
+            raise TypeError("Each row of the matrix must have the same size")
+        if type(div) not in (int, float):
+            raise TypeError("div must be a number")
+        row = []
+        for element in mat:
+            if div == 0:
+                raise ZeroDivisionError("division by zero")
+            if type(element) not in (int, float):
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            row.append(round(element / div, 2))
+        divs.append(row)
+    return divs
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("test/2-matrix_divided.txt")
