aca62417c6880c55ec03677521e8ffc5138d9778
yassineboucham <yassineboucham11@gamil.com>
2024-01-06 02:30:08 +0000

    12

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,14 +1,25 @@
 #!/usr/bin/python3
+"""This module defines a Rectangle class."""
+
+
 class Rectangle:
+    """A class representing a rectangle."""
     __width = 0
     __height = 0
 
+    def __init__(self, width=0, height=0):
+        """Initializes a new Rectangle instance."""
+        self.__width = width
+        self.__height = height
+
     @property
     def width(self):
+        """Getter method for the width attribute."""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Setter method for the width attribute."""
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -17,16 +28,14 @@ class Rectangle:
 
     @property
     def height(self):
+        """Getter method for the height attribute."""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """Setter method for the height attribute."""
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-
-    def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
