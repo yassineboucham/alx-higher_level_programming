d7b6af479302df006756e393516acbf37dd127ab
yassineboucham <yassineboucham11@gamil.com>
2024-01-06 02:21:08 +0000

    12

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 class Rectangle:
-    __width = 0
-    __height = 0
-
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
