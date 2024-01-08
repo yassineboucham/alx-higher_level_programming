173a18a412246affe00bb1f99dbc3b24d7b8e1ad
yassineboucham <yassineboucham11@gamil.com>
2024-01-08 16:42:42 +0000

    7

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -14,3 +14,7 @@ class BaseGeometry:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("7-base_geometry.txt")
