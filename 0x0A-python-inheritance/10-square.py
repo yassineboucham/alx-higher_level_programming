ee7a2bde4e1e698bddd143bb2de24179b5d226e8
yassineboucham <yassineboucham11@gamil.com>
2024-01-10 13:24:00 +0000

    aa

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""task 10"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    """ class Square that inherits from Rectangle"""
+    def __init__(self, size):
+        """init"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size,size)
+
+    def area(self):
+        """area"""
+        return self.__size ** 2
