e81dbe1c6d8bcf34032338126b26ec1660fcf637
yassineboucham <yassineboucham11@gamil.com>
2024-01-10 22:42:52 +0000

    aa

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+
 def read_file(filename=""):
     with open(filename, encoding="utf_8") as f:
         return f.read()
+    # The following line will never be reached, so it can be removed
+    # f.closed
