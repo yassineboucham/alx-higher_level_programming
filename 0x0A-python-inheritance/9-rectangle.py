344b297b2a8b9ec0d131b3758c18be13981c9660
yassineboucham <yassineboucham11@gamil.com>
2024-01-10 11:45:44 +0000

    ayy

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""task 8"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """a class Rectangle that inherits from BaseGeometry"""
+    def __init__(self, width, height):
+        """init"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def print(self):
+        """print"""
+        print("[Rectangle] {}/{}".format(self.__width, self.__height))
+
+    def __str__(self):
+        """str"""
+        return ("[Rectangle] {}/{}".format(self.__width, self.__height)) 
+
+    def area(self):
+        """w * h"""
+        return (self.__width * self.__height)
