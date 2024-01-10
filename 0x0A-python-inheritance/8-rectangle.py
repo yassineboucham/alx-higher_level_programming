480c22b1a3d7a19ac3b0220e24212581dbcb5dfd
yassineboucham <yassineboucham11@gamil.com>
2024-01-10 11:31:01 +0000

    8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -15,13 +15,16 @@ class BaseGeometry:
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
 
+
 if __name__ == "__main__":
     import doctest
     doctest.testfile("7-base_geometry.txt")
 
+
 class Rectangle(BaseGeometry):
     """a class Rectangle that inherits from BaseGeometry"""
     def __init__(self, width, height):
+        """init"""
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
