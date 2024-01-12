ff746568e77f31d60bac1bf329ee5f4b4eddc04a
yassineboucham <yassineboucham11@gamil.com>
2024-01-12 11:59:25 +0000

    ayy

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""task 6"""
+import json
+
+
+def load_from_json_file(filename):
+    """json"""
+    with open(filename, "r", encoding="utf_8") as f:
+        return json.load(f)
