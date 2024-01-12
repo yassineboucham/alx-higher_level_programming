75f217586d6be83820e28f8ba142a4df11a3df2c
yassineboucham <yassineboucham11@gamil.com>
2024-01-12 12:28:46 +0000

    yarbi tkhdem

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""task 7"""
+import sys
+
+
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+save_to_json_file(sys.argv[1:], "add_item.json")
+load_from_json_file("add_item.json")
