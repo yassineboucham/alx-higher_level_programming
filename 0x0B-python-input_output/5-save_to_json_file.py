23d8e5629bd79c8cce9aed53834c06578ecc5d8d
yassineboucham <yassineboucham11@gamil.com>
2024-01-12 11:47:31 +0000

    alla

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""task 5"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """json"""
+    with open(filename, "w", encoding="utf_8") as f:
+        f.write(json.dumps(my_obj))
