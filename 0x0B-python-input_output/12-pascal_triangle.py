4fb27a06291d9271055a31e009b1dea606862dab
yassineboucham <yassineboucham11@gamil.com>
2024-01-13 15:50:53 +0000

    aaal

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""tan9alt"""
+
+
+def pascal_triangle(n):
+    """no9Al"""
+    if n <= 0:
+        return []
+    list = []
+    while len(list) != n:
+        tri = list[-1]
+        tmp = [1]
+        for i in range(len(tri) - 1):
+            tmp.append(tri[i] + tri[i + 1])
+        tmp.append(1)
+        list.append(tmp)
+    return list
+        
