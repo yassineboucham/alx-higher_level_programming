5e153d38830c5d5f20ed1345c0e628c720354574
yassineboucham <yassineboucham11@gamil.com>
2024-01-13 15:01:19 +0000

    all

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""task 8"""
+
+
+class Student:
+    """class std"""
+    def __init__(self, first_name, last_name, age):
+        """init"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """to json"""
+        return self.__dict__
