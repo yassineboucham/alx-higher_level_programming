6cc90ee0a27f7bcc64f15ff1530abcee72d9c351
yassineboucham <yassineboucham11@gamil.com>
2024-01-13 15:16:17 +0000

    ms3ood

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""task 10"""
+
+
+class Student:
+    """class std"""
+    def __init__(self, first_name, last_name, age):
+        """init"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """to json"""
+        try:
+            for attr in attrs:
+                if type(attr) is not str:
+                    return self.__dict__
+        except Exception:
+            return self.__dict__
+        my_dict = dict()
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                my_dict[key] = value
+        return my_dict
