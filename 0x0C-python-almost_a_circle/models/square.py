36d87ee9834ac60069d762e421f4d1a46eff842a
yassineboucham <yassineboucham11@gamil.com>
2024-01-16 18:43:15 +0000

    hh

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -32,3 +32,16 @@ class Square(Rectangle):
         super().validation("width", value)
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """update """
+        if len(args) > 0:
+            self.id = args[0]
+        if len(args) > 1:
+            self.size = args[1]
+        if len(args) > 2:
+            self.x = args[2]
+        if len(args) > 3:
+            self.y = args[3]
+        for key, value in kwargs.items():
+            setattr(self, key, value)
