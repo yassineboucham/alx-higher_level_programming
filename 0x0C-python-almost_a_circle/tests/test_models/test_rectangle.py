b36c54c196415a6f26861bb2845c3ce792971192
yassineboucham <yassineboucham11@gamil.com>
2024-01-16 17:33:16 +0000

    yarbi i khdem

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+
+test rectangle
+
+
+"""
+from models.base import Base
+from models.rectangle import Rectangle
+import unittest
+
+
+class TesrBaseMethods(unittest.TestCase):
+    """Suite to test Base class"""
