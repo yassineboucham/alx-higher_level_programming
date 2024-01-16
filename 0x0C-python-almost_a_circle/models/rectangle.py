9c67f2a458b055d67c82b4c6115fbd23ae4e6b17
yassineboucham <yassineboucham11@gamil.com>
2024-01-16 19:05:40 +0000

    ali

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -136,6 +136,10 @@ class Rectangle(Base):
     def to_dictionary(self):
         """to_dictionary"""
         dic = {
-            "id": self.id, "width": self.width, "height": self.height, "x": self.x, "y": self.y
-            }
+            "id": self.id,
+            "width": self.width,
+            "height": self.height,
+            "x": self.x,
+            "y": self.y
+        }
         return dic
