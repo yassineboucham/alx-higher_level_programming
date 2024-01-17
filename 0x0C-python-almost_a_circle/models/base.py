189b51ba15b005b8df1c3e5a5487d491a555da76
yassineboucham <yassineboucham11@gamil.com>
2024-01-17 04:21:10 +0000

    all

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,6 +5,7 @@ base
 
 
 """
+import json
 
 
 class Base:
@@ -18,3 +19,9 @@ class Base:
         else:
             Base.__nb_objects += 1
             Base.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """to_json_string"""
+        dp = json.dumps(list_dictionaries)
+        if dp is not (None, []):
+            return dp
