f058c3e8447393dee5b4152c3db39bd4d01f13f8
yassineboucham <yassineboucham11@gamil.com>
2024-04-03 18:46:29 +0100

    tesks 4

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,2 @@
 -- script that creates a table called first_table
-CREATE TABLE first_table (
-    id INT
-    name VARCHAR(256)
-);
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
