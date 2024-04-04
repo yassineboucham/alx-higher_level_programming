e1f588c9636866f92ca6a089517adc56d084e5c6
yassineboucham <yassineboucham11@gamil.com>
2024-04-04 01:32:54 +0100

    15

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- script that computes the score average of all records in the table
-SELECT score, SUM(score) AS number FROM second_table GROUP BY score ORDER BY score DESC;
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY score DESC;
