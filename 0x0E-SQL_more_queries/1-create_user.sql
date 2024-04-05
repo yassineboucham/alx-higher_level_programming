4a4fe518f92d3826f50c188597d3e6ef6a65475c
yassineboucham <yassineboucham11@gamil.com>
2024-04-05 15:07:46 +0100

    t 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,4 @@
 -- script that creates the MySQL server user user_0d_1.
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON example_database.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
