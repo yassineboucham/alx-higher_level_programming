2934f01e0e6047b454a53291b2c73b45b30bb57e
yassineboucham <yassineboucham11@gamil.com>
2024-04-05 02:18:34 +0100

    tasck 0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,3 @@
 --  script that lists all privileges of the MySQL users user_0d_1 and user_0d_2
-SHOW GRANTS FROM user_0d_1@localhost;
-SHOW GRANTS FROM user_0d_2@localhost;
- 
+SHOW GRANTS FROM 'user_0d_1'@'localhost';
+SHOW GRANTS FROM 'user_0d_2'@'localhost';
