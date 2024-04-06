a54d55b1fdc878f0cd415474617a953263b4bd71
yassineboucham <yassineboucham11@gamil.com>
2024-04-06 15:06:49 +0100

    7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,6 +3,6 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
     id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
     state_id INT NOT NULL,
-    FOREIGN KEY(state_id) REFERENCES TO hbtn_0d_usa.state(id),
+    FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.state(id),
     name VARCHAR(256) NOT NULL
     );
