db6a36338088a3ee04b684f2140012db5537fa15
yassineboucham <yassineboucham11@gamil.com>
2024-04-09 15:23:16 +0100

    js7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -5,6 +5,6 @@ const { argv } = require('node:process');
 if (argv.lenght === 2) {
     console.log('Missing number of occurrences')
 } 
-for (let i='0'; i<=argv; i++) {
+for (let i='0'; i<=argv[2]; i++) {
     console.log('C is fun')
 }
