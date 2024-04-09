9a2c90e8802d965a7db26cd842500781e5846120
yassineboucham <yassineboucham11@gamil.com>
2024-04-09 10:32:31 +0100

    js2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,10 +4,8 @@ const { argv } = require('node:process');
 
 if (argv.length <= 2) {
     console.log('No argument');
-}
-else if (argv.length === 3) {
+} else if (argv.length === 3) {
     console.log('Argument found');
-}
-else {
-    console.log("Argument found");
+} else {
+    console.log('Argument found');
 }
