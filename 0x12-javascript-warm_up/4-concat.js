7605de50b49f9b6e9f9b00fd8ab7b5900beab3f8
yassineboucham <yassineboucham11@gamil.com>
2024-04-09 12:05:19 +0100

    js4

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+const { argv } = require('node:process');
+
+if (argv.length <= 2){
+    console.log('undefined is undefined');
+} else if (argv === 3){
+    console.log(argv[2], 'is undefined');
+} else {
+    console.log(argv[2], argv[3]);
+}
