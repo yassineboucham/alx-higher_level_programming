4331f510f8be7e9e4fec5ad154723318da315c32
yassineboucham <yassineboucham11@gamil.com>
2024-04-09 14:23:36 +0100

    js5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,8 +2,8 @@
 
 const { argv } = require('node:process');
 
-if (Number(argv[2])) {
-    console.log('My number:', Math.floor(Number(argv[2])));
+if (!Number(argv[2])) {
+  console.log('Not a number');
 } else {
-    console.log('Not a number');
+  console.log('My number:', Math.floor(Number(argv[2])));
 }
