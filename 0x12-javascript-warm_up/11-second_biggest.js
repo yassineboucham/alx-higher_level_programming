1a84c5ed7f962f891ca9eef52f0dee7811db9028
yassineboucham <yassineboucham11@gamil.com>
2024-04-11 11:46:00 +0100

    js11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+
+const { argv } = require('node:process');
+
+if (argv.length <= 3) {
+  console.log(0);
+} else {
+  const arrayOfNumbers = argv.slice(2).map(val => Number(val)).sort((a, b) => b - a);
+  console.log(arrayOfNumbers[1]);
+}
