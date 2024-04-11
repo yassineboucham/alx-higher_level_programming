d175819a892a08f8d60a8d4895c939c2716df426
yassineboucham <yassineboucham11@gamil.com>
2024-04-12 00:42:52 +0100

    8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,18 @@
 #!/usr/bin/node
-
+/*
 exports.esrever = function (list) {
     let newlist = [];
         for (let i = list.length - 1; i >= 0; i--) {
             newlist.push(list[i]);
         }
+    return newlist;
+};
+*/
+
+exports.esrever = function (list) {
+    let newlist = [];
+    for (let i = list.length - 1; i >= 0; i--) {
+        newlist.push(list[i]);
+    }
     return newlist;
 };
