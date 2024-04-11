b1083b14c949d7b7e4857d291e13da30de760fa0
yassineboucham <yassineboucham11@gamil.com>
2024-04-12 00:53:35 +0100

    6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
 
 module.exports = class Square extends require('./5-square.js') {
-    constructor(size) {
-        super(size, size);
-    }
-
     charPrint(c) {
         if (c === undefined) {
             this.print();
