1fb1e630d45eaae1de57f69b725234d7150988d0
yassineboucham <yassineboucham11@gamil.com>
2024-04-12 00:57:49 +0100

    6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,10 +1,17 @@
 #!/usr/bin/node
+// Specifies that this script should be executed with the Node.js interpreter
+
+// Exporting a class named Square
 module.exports = class Square extends require('./5-square.js') {
-    charPrint (c) {
+    // Method to print a square made of a specific character
+    charPrint(c) {
+      // If no character is provided, call the print method of the parent class
       if (c === undefined) {
         this.print();
       } else {
+        // Print the square using the provided character
         for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
       }
     }
   };
+  
