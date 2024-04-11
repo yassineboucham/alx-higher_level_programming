56b729328653f97e34dffeabcf928ccaa8ee9001
yassineboucham <yassineboucham11@gamil.com>
2024-04-12 00:44:51 +0100

    7

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -8,6 +8,7 @@ exports.nbOccurences = function (list, searchElement) {
     return conter;
 };
 */
+
 exports.nbOccurences = function (list, searchElement) {
   return list.reduce((count, current) => current === searchElement ? count + 1 : count, 0);
 };
