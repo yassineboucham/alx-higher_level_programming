64cb48fba1976145bf007a5c8f6d52412d64afad
yassineboucham <yassineboucham11@gamil.com>
2024-04-11 11:45:16 +0100

    js10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+
+const { argv } = require('node:process');
+
+const calcFactorail = function (num) {
+  if (!num || num === 1) {
+    return (1);
+  } else {
+    return num * calcFactorail(num - 1);
+  }
+};
+
+console.log(calcFactorail(Number(argv[2])));
