d575254fbefdf6560a5be7e3ae2485d559240062
yassineboucham <yassineboucham11@gamil.com>
2024-04-12 00:44:41 +0100

    7

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,9 +1,13 @@
 #!/usr/bin/node
-
+/*
 exports.nbOccurences = function (list, searchElement) {
     conter = 0;
     list.forEach(element => {
         if (element === searchElement)  conter++;
     });
     return conter;
+};
+*/
+exports.nbOccurences = function (list, searchElement) {
+  return list.reduce((count, current) => current === searchElement ? count + 1 : count, 0);
 };
