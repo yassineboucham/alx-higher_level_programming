7f9fdfa316ad9e4d9300fa3015af2a1c81209bc8
yassineboucham <yassineboucham11@gamil.com>
2024-04-11 20:41:00 +0100

    js3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,18 +1,4 @@
 #!/usr/bin/node
-/*
-module.exports = class Rectangle {
-    constructor (w, h) {
-      if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
-    }
-
-    print() {
-        for(let i=0; i<this.height; i++) {
-            console.log('X'.repeat(this.width));
-        }
-    }
-  };
-*/
-
 module.exports = class Rectangle {
     constructor (w, h) {
       if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
@@ -22,3 +8,4 @@ module.exports = class Rectangle {
       for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
     }
   };
+  
