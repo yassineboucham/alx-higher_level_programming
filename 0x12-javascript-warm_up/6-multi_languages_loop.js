b58d2412882dead4627b4bff9c728f24b7f72b84
yassineboucham <yassineboucham11@gamil.com>
2024-04-11 11:41:35 +0100

    js6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,17 @@
 #!/usr/bin/node
 
+/*
 let arr = ["C is fun", "Python is cool", "JavaScript is amazing"];
 
 for (let i=0; i<3; i++) {
     console.log(arr[i]);
 }
+*/
+
+const myArray = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+
+myArray.forEach(elem => console.log(elem));
