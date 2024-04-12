077f16965835ada72b2c3b1bf60e83d632cb9914
yassineboucham <yassineboucham11@gamil.com>
2024-04-12 12:09:32 +0100

    9

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,9 +1,5 @@
 #!/usr/bin/node
 
 exports.logMe = function (item) {
-    i=0;
-    item.forEach(element => {
-        console.log(i, ':', element);
-        i++;
-    });
+    console.log(i++, ':', item);
 };
