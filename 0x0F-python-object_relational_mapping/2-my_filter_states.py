42748ac5c51dc137e0ff5c5e704f820916d83101
yassineboucham <yassineboucham11@gamil.com>
2024-04-13 22:17:45 +0100

    2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    username=sys.argv[1]
+    passwd=sys.argv[2]
+    dbname=sys.argv[3]
+    searched=sys.argv[4]
+
+    db=MySQLdb.connect(host="localhost",
+                       user=username,
+                       passworld=passwd,
+                       database=dbname,
+                       port=3306)
+
+    c = db.cursor()
+    c.execute("SELECT * FROM states WHERE name LIKE '{}' ORDER BY states.id".format(searched))
+
+    for row in c.fetchall():
+        print(row)
