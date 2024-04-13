7e47b25a5ad0c5cd1b701973b6b36b86f01f8c77
yassineboucham <yassineboucham11@gamil.com>
2024-04-13 15:19:10 +0100

    all

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
-"""Lists states"""
 
+"""
+
+Print the row of a table using MySQLdb
+
+"""
 import sys
-import MySQLdb # type: ignore
+import MySQLdb
 
 if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
 
-    db = MySQLdb.connect(host = "localhost",
-                         user = username,
-                         password = password,
-                         database = db_name,
-                         port = 3306)
+    db = MySQLdb.connect(host="localhost",
+                         user=username,
+                         password=password,
+                         database=db_name,
+                         port=3306)
     c = db.cursor()
     c.execute("SELECT * FROM states ORDER BY id")
 
