c0518b1b552aa5e35fd1394f47fd6a3c5a183e0c
yassineboucham <yassineboucham11@gamil.com>
2024-04-15 15:26:52 +0100

    all

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,25 +1,31 @@
 #!/usr/bin/python3
 
-"""sql inj"""
+"""
 
+Displays values in the states table based
+on argument. But this time sould be safe
+from MySQL injections!
+
+"""
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
     username = sys.argv[1]
-    passwd = sys.argv[2]
+    password = sys.argv[2]
     db_name = sys.argv[3]
     searched = sys.argv[4]
 
     db = MySQLdb.connect(host="localhost",
                          user=username,
-                         password=passwd,
+                         password=password,
                          database=db_name,
-                         port=3306)
+                         port=3306
+                         )
 
     c = db.cursor()
-    c.execute("SELECT * FROM states WHERE LIKE %s ORDERE BY id",
-               (searched, ))
+    c.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id",
+              (searched, ))
 
     for row in c.fetchall():
         print(row)
