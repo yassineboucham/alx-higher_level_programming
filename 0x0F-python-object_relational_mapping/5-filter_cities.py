59e81576ddc841ee8925daafd3bed5f97ae682e0
yassineboucham <yassineboucham11@gamil.com>
2024-04-15 15:49:54 +0100

    py5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,19 +1,37 @@
 #!/usr/bin/python3
 
-"""sql inj"""
+"""
 
-import sys
+Lists all cities of that state,
+based on name argument.
+
+"""
+
+from sys import argv
 import MySQLdb
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    passwd = sys.argv[2]
-    db_name = sys.argv[3]
+    username = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+    searched = argv[4]
 
-db = MySQLdb.connect(host="localhost",
-                   user=username,
-                   password=passwd,
-                   database=db_name,
-                   port=3306
-                   )
-c = db.cursor()
+    db = MySQLdb.connect(host="localhost",
+                         user=username,
+                         password=password,
+                         database=db_name,
+                         port=3306
+                         )
+
+    c = db.cursor()
+    c.execute("""
+        SELECT c.id, c.name, s.name FROM cities AS c
+                                              JOIN states AS s
+                                                ON c.state_id = s.id
+                                             WHERE s.name = %s
+                                          ORDER BY c.id
+                                          """, (searched, ))
+    print_str = ""
+    for row in c.fetchall():
+        print_str += row[1] + ", "
+    print(print_str[:-2])
