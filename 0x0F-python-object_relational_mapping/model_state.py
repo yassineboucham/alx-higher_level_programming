9cb49921c64e4fbad94e3cb29d59b72d7f7aac48
yassineboucham <yassineboucham11@gamil.com>
2024-04-15 16:06:44 +0100

    py6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python3
+
+"""
+
+State class
+
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    The State class will abstract the implementation of states table
+    """
+    __tablename__ = "states"
+
+    id = Column(Integer,
+                autoincrement=True,
+                primary_key=True,
+                unique=True,
+                nullable=False)
+    name = Column(String(128), nullable=False)
+
+    def __repr__(self):
+        return f"{self.id}: {self.name}"
