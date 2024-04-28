664facea2ffb810feb9a2430b0c4455be2213dba
yassineboucham <yassineboucham11@gamil.com>
2024-04-28 02:55:49 +0100

    4

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#GET request to the URL, and displays the body of the response
-#A header variable X-School-User-Id must be sent with the value 98
-curl $1 -X GET -H X-School-User-Id: 98
+#GET request to the URL
+curl $1 -X GET -H X-School-User-Id:98
