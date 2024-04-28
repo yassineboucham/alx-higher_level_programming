3e6a834396059b86113438f82b1263e2850b8cfc
yassineboucham <yassineboucham11@gamil.com>
2024-04-28 17:52:10 +0100

    end

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,13 +2,28 @@
 
 def find_peak(list_of_integers):
     """function that finds a peak in a list of unsorted integers"""
-    num = list_of_integers
-    s = len(list_of_integers)
-
-    if s == 0:
+    if not list_of_integers:
         return None
-    if s == 1:
-        return num[1]
-    for i in range(s):
-        if (i == 0 or num[i-1] <= num[i]) and (i == n-1 or num[i] >= num[i+1]):
-            return num[i]
+    # Binary search-like approach
+    low = 0
+    high = len(list_of_integers) - 1
+
+    while low < high:
+    mid = (low + high) // 2
+
+    # Check if mid is a peak
+    if (mid == 0 or list_of_integers[mid] >= list_of_integers[mid - 1]) and \
+        (mid == len(list_of_integers) - 1 or list_of_integers[mid] >= list_of_integers[mid + 1]):
+        return list_of_integers[mid]
+
+    # If the mid element is smaller than its left neighbor,
+    # then a peak element must exist on the left side
+    elif mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+        high = mid - 1
+
+    # Otherwise, a peak element must exist on the right side
+    else:
+        low = mid + 1
+
+    # If the loop exits, it means the peak is at the remaining single element
+    return list_of_integers[low]
