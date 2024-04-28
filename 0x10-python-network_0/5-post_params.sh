13823a8b9b63ae2effefeb055bbc3950e861b7e9
yassineboucham <yassineboucham11@gamil.com>
2024-04-28 03:12:56 +0100

    5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #post curl
-curl -s -X POST -d "email:test@gmail.com&subject:I%20will%20always%20be%20here%20for%20PLD" $1
+curl -s -X POST -d "email=test@gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD" $1
