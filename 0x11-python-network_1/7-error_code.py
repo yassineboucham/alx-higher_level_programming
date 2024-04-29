184bbe6c7116cb275d5418ebcac9d0936f46cdca
yassineboucham <yassineboucham11@gamil.com>
2024-04-29 18:06:11 +0100

    7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -11,8 +11,8 @@ if __name__ == "__main__":
     url = sys.argv[1]
     if url:
         req = requests.get()
-        if req.status_code < 400:
+        if req.status_code != 200:
+            print("Error code: {}".format(req.status_code))
+        else:
             value = req.text()
             print(value)
-        else:
-            print("Error code: {}".format(req.status_code))
