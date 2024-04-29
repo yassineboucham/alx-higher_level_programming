f181cc10bcd005479e5c6a5bdcec4228db4e93a1
yassineboucham <yassineboucham11@gamil.com>
2024-04-29 18:02:41 +0100

    7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""
+ a Python script that takes in a URL,
+ sends a request to the URL and displays the body of the response.
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    if url:
+        req = requests.get()
+        if req.status_code == 200:
+            value = req.text()
+            print(value)
+        else:
+            print("Error code: {}".format(req.status_code))
