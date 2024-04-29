65166b6297daf18098d956809a90099eccdea9d1
yassineboucham <yassineboucham11@gamil.com>
2024-04-29 17:31:37 +0100

    5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""
+Python script that takes in a URL,
+sends a request to the URL and displays
+the value of the variable X-Request-Id in the response header
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    if url:
+        req = requests.get(url)
+        print(req.headers['X-Request-Id'])
