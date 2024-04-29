791245a92d073d240ac0f8acace14ce59a36f9e5
yassineboucham <yassineboucham11@gamil.com>
2024-04-29 16:11:28 +0100

    4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""
+ Python script that fetches https://alx-intranet.hbtn.io/status
+"""
+import urllib.request
+
+
+with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as html:
+    fetch = html.read()
+print("Body response:")
+print("\t- type: {}".format(type(fetch)))
+print("\t- content: {}".format(fetch.decode("utf-8")))
