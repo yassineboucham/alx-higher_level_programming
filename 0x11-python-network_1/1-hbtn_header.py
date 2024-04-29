f234bdc207057aabd4a891e4eb5cf4dfe607b100
yassineboucham <yassineboucham11@gamil.com>
2024-04-29 01:49:13 +0100

    1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 import sys
 import urllib.request
-url = sys.argv[1]
-if url:
-   with urllib.request.urlopen(url) as html:
-       value = html.getheader('X-Request-Id')
-print('{}'.format(value))
+if __name__ == "__main__":
+    url = sys.argv[1]
+    if url:
+        with urllib.request.urlopen(url) as html:
+            value = html.getheader('X-Request-Id')
+        print('{}'.format(value))
