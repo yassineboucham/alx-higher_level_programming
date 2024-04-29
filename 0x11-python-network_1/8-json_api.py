d0e9b2f39198d604be5acdfc3451220a9213fe47
yassineboucham <yassineboucham11@gamil.com>
2024-04-29 23:56:04 +0100

    8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""
+ Python script that takes in a letter and sends a POST
+ request to http://0.0.0.0:5000/search_user
+ with the letter as a parameter.
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    q = sys.argv[1]
+    if not q:
+        q = ""
+    data = {"q": q}
+    res = requests.post("http://0.0.0.0:5000/search_user", data)
+    try:
+        value = res.json()
+        if not value:
+            print("No result")
+        else:
+            id = value.get('id')
+            name = value.get('name')
+            print("[{}] {}".format(id, name))
+    except ValueError as v:
+        print("Not a valid JSON")
