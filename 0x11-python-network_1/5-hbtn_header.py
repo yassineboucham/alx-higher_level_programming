5fdb1f90860ae097ac986b7b15557108e45465c8
yassineboucham <yassineboucham11@gamil.com>
2024-04-29 17:34:14 +0100

    5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -12,4 +12,5 @@ if __name__ == "__main__":
     url = sys.argv[1]
     if url:
         req = requests.get(url)
-        print(req.headers['X-Request-Id'])
+        value = req.headers['X-Request-Id']
+        print(value)
